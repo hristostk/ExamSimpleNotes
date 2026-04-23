336bc2adac96f5e050de2778e86ba1bcdec44eb9
hristostk <hristostk@gmail.com>
2026-04-23 10:31:47 +0200

    Initial commit

---
+++ Models/NoteDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SimpleNotesExam.Models
+{
+    internal class NoteDto
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+    }
+}
